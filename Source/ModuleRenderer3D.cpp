72423ee20e88173be26a5ab591fa87c538a3934c
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-02-12 12:20:48 +0100

    Todo comment

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -148,6 +148,8 @@ update_status ModuleRenderer3D::PreUpdate(float dt)
 // PostUpdate present buffer to screen
 update_status ModuleRenderer3D::PostUpdate(float dt)
 {
+
+	// TODO
 	if (editor_camera != nullptr && editor_camera->GetViewportTexture() != nullptr)
 	{
 		editor_camera->GetViewportTexture()->Bind();
