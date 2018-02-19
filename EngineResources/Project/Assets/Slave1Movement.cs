479a6502650f03c192de258e4e1aabe3b4432232
Trodek <ibanmas@gmail.com>
2018-02-19 23:26:57 +0100

    slave1 movement improved (not tested)

--- EngineResources/Project/Assets/Slave1Movement.cs
+++ EngineResources/Project/Assets/Slave1Movement.cs
@@ -3,18 +3,32 @@ using TheEngine;
 public class Slave1Movement {
 
 	TheTransform trans;
-	public float rotate_speed = 1.0f;
-	public float vertical_thrust = 5.0f;
-	public float throttle_increment = 1.0f;
-	
 	public int controller_sensibility = 100;
 	
+	public float rotate_speed = 1.0f;
+	public float vertical_thrust = 5.0f;
+	public float throttle_increment = 0.1f;
 	public float max_vel = 10.0f;
-	private float curr_vel = 0.0f;
+	public float acceleration = 1.0f;
 	
-
-	void Start () {
+	private float curr_vel = 0.0f;
+	private float vel_percent = 0.0f; //from 1.0 to -1.0
+	
+	
+	private enum ThrottleState
+	{
+		TS_NULL,
+		TS_FORDWARD,
+		TS_STATIC,
+		TS_BACKWARD,
+	}
+	
+	ThrottleState throttle = TS_NULL;
+	
+	void Start () 
+	{
 		trans = TheGameObject.Self.GetComponent<TheTransform>();
+		throttle = TS_STATIC;
 	}
 	
 	void Update () 
@@ -29,8 +43,8 @@ public class Slave1Movement {
 		int ljoy_right = TheInput.GetControllerJoystickMove(0,"LEFTJOY_RIGHT");
 		int ljoy_left = TheInput.GetControllerJoystickMove(0,"LEFTJOY_LEFT");
 		
-		int right_trigger = TheInput.GetControllerJoystickMove(0,"RIGHT_TRIGGER");
-		int left_trigger = TheInput.GetControllerJoystickMove(0,"LEFT_TRIGGER");
+		//int right_trigger = TheInput.GetControllerJoystickMove(0,"RIGHT_TRIGGER");
+		//int left_trigger = TheInput.GetControllerJoystickMove(0,"LEFT_TRIGGER");
 		
 		if(ljoy_up > controller_sensibility)
 		{
@@ -96,19 +110,87 @@ public class Slave1Movement {
 			trans.LocalRotation = new_rot;
 		}
 		
-		if(right_trigger > controller_sensibility)
+		if(TheInput.IsKeyDown("CONTROLLER_R3") && !TheInput.IsKeyDown("CONTROLLER_L3"))
 		{
-			float move_percentage = (float)(right_trigger - controller_sensibility)/(float)(TheInput.MaxJoystickMove - controller_sensibility);
-			if(curr_vel < max_vel)
-				curr_vel += move_percentage*throttle_increment;
+			switch(throttle)
+			{
+				case ThrottleState.TS_FORDWARD:
+					break;
+				case ThrottleState.TS_STATIC:
+					throttle = ThrottleState.TS_FORDWARD;
+					break;
+				case ThrottleState.TS_BACKWARD:
+					break;
+			}
 		}
 		
-		if(left_trigger > controller_sensibility)
+		if(TheInput.IsKeyRepeat("CONTROLLER_R3") && !TheInput.IsKeyRepeat("CONTROLLER_L3"))
 		{
-			float move_percentage = (float)(left_trigger - controller_sensibility)/(float)(TheInput.MaxJoystickMove - controller_sensibility);
-			if(curr_vel > -max_vel)
-				curr_vel -= move_percentage*throttle_increment;
+			switch(throttle)
+			{
+				case ThrottleState.TS_FORDWARD:
+					if(vel_percent<1.0f)
+						vel_percent+=throttle_increment*Time.DeltaTime;
+					break;
+				case ThrottleState.TS_STATIC:
+					
+					break;
+				case ThrottleState.TS_BACKWARD:
+					if(vel_percent<0.0f)
+						vel_percent+=throttle_increment*Time.DeltaTime;
+					else
+					{
+						throttle = ThrottleState.TS_STATIC;
+						vel_percent = 0.0f;
+					}
+					break;
+			}
 		}
+		
+		if(TheInput.IsKeyDown("CONTROLLER_L3") && !TheInput.IsKeyDown("CONTROLLER_R3"))
+		{
+			switch(throttle)
+			{
+				case ThrottleState.TS_FORDWARD:
+					
+					break;
+				case ThrottleState.TS_STATIC:
+					throttle = ThrottleState.TS_BACKWARD;
+					break;
+				case ThrottleState.TS_BACKWARD:
+					
+					break;
+			}
+		}
+		
+		if(TheInput.IsKeyRepeat("CONTROLLER_L3") && !TheInput.IsKeyRepeat("CONTROLLER_R3"))
+		{
+			switch(throttle)
+			{
+				case ThrottleState.TS_FORDWARD:
+					if(vel_percent>0.0f)
+						vel_percent-=throttle_increment*Time.DeltaTime;
+					else
+					{
+						throttle = ThrottleState.TS_STATIC;
+						vel_percent = 0.0f;
+					}
+					break;
+				case ThrottleState.TS_STATIC:
+					
+					break;
+				case ThrottleState.TS_BACKWARD:
+					if(vel_percent>-1.0f)
+						vel_percent-=throttle_increment*Time.DeltaTime;
+					break;
+			}
+		}
+		
+		float target_vel = max_vel * vel_percent;
+		if(curr_vel<target_vel) curr_vel+=acceleration*Time.DeltaTime;
+		else if(curr_vel>target_vel) curr_vel-=acceleration*Time.DeltaTime;
+		
+		if(vel_percent == 0.0f) curr_vel = 0.0f;
 		
 		TheVector3 new_vel_pos = trans.LocalPosition;
 		new_vel_pos += trans.ForwardDirection*curr_vel*Time.DeltaTime;
