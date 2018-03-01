f95ed71cdcbb0c3b45196ada163d5b5cb10757f1
Trodek <ibanmas@gmail.com>
2018-03-01 01:42:01 +0100

    component GOAP wip

--- Source/ComponentGOAPAgent.h
+++ Source/ComponentGOAPAgent.h
@@ -2,6 +2,11 @@
 #define __COMPONENTGOAPAGENT__
 
 #include "Component.h"
+#include <vector>
+
+class GOAPAction;
+class GOAPGoal;
+class GOAPVariable;
 
 class ComponentGOAPAgent : public Component
 {
@@ -10,6 +15,13 @@ public:
 	
 	void Save(Data& data) const;
 	void Load(Data& data);
+
+	bool Update();
+
+public:
+	std::vector<GOAPAction*> goals;
+	std::vector<GOAPAction*> actions;
+	std::vector<GOAPVariable*> blackboard;
 };
 
 #endif // !__COMPONENTGOAPAGENT__
