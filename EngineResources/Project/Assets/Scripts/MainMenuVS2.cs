9fff166d166174ffa36348cc7694c6082e71a168
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-03-11 23:25:30 +0100

    Working on vs menu

---
+++ EngineResources/Project/Assets/Scripts/MainMenuVS2.cs
@@ -0,0 +1,22 @@
+using TheEngine;
+
+public class MainMenuVS2 
+{
+	public TheGameObject campaign_button_go;
+	public TheGameObject settings_button_go;
+	public TheGameObject exit_button_go;
+
+	TheButton campaign_button = null;
+	TheButton settings_button = null;
+	TheButton exit_button = null;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
