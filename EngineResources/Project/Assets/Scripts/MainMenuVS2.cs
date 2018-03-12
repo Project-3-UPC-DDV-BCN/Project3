6a567149fb7e54d970132ebadec1d831b927f8ce
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-03-12 17:05:20 +0100

    Main menu loading vs final scene

--- EngineResources/Project/Assets/Scripts/MainMenuVS2.cs
+++ EngineResources/Project/Assets/Scripts/MainMenuVS2.cs
@@ -7,6 +7,7 @@ public class MainMenuVS2
 	public TheGameObject settings_button_go;
 	public TheGameObject exit_button_go;
 	public TheGameObject explanation_text_go;
+	public TheGameObject loading_text_go;
 
 	TheRectTransform campaign_rect = null;
 	TheRectTransform settings_rect = null;
@@ -37,6 +38,9 @@ public class MainMenuVS2
 			if(explanation_text != null)
 				explanation_text.Text = "";
 		}
+
+		if(loading_text_go != null)
+			loading_text_go.SetActive(false);
 	}
 	
 	void Update ()
@@ -45,7 +49,10 @@ public class MainMenuVS2
 		{
 			if(campaign_rect.OnClickUp)
 			{
-				TheApplication.LoadScene("VS2");
+				if(loading_text_go != null)
+					loading_text_go.SetActive(false);
+
+				TheApplication.LoadScene("VerticalSlice_NoPanelUIGood_guillemdominguez");
 				return;
 			}
 
