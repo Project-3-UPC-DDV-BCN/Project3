7ddf979608e1995647ca5d40a296ca397e816485
Roger Tello Araez <rogerteello97@gmail.com>
2018-03-28 21:46:46 +0200

    destruction time limit

--- EngineResources/Project/Assets/Scripts/ShipDestruction.cs
+++ EngineResources/Project/Assets/Scripts/ShipDestruction.cs
@@ -7,6 +7,9 @@ public class ShipDestruction
 {
 
 	public float explosion_v; 
+	public float time_to_destroy; 
+
+	private float destroy_timer; 
 
 	List<TheGameObject> ship_parts; 
 
@@ -22,18 +25,29 @@ public class ShipDestruction
 		ship_parts = new List<TheGameObject>(); 
 		need_boom = false; 
 		exploted = false; 
+		destroy_timer = 0; 
 	}
 	
 	void Update ()
 	{
 		if(TheInput.IsKeyDown("RIGHT_ARROW"))
 		{			
-			TheConsole.Log("ExploteNow"); 
 			FillPartList(); 
 			SetPartsDirection(); 		
 			exploted = true; 
 		}
-				 	
+
+		if(exploted)
+		{
+			destroy_timer++; 
+
+			if(destroy_timer > time_to_destroy * 100)
+				DeleteShipParts(); 
+		}
+
+		TheConsole.Log("---");
+		TheConsole.Log(destroy_timer); 
+		TheConsole.Log(time_to_destroy);  	
 	}
 
 	void FillPartList()
@@ -41,6 +55,15 @@ public class ShipDestruction
 		for(int i = 0; i < TheGameObject.Self.GetChildCount(); i++)
 		{			
 			ship_parts.Add(TheGameObject.Self.GetChild(i)); 
+		}
+	}
+
+	void DeleteShipParts()
+	{
+		for(int i = 0; i < TheGameObject.Self.GetChildCount(); i++)
+		{			
+			ship_parts.Remove(TheGameObject.Self.GetChild(i)); 
+			ship_parts[i].SetActive(false); 
 		}
 	}
 
@@ -52,33 +75,35 @@ public class ShipDestruction
 		{
 			TheVector3 direction = transform.ForwardDirection.Normalized; 
 
-			float rand = TheRandom.RandomRange(-5,5); 
-			direction.x = rand; 
+			float randx = TheRandom.RandomRange(-5,5); 
+			direction.x = randx; 
 			
-			rand = TheRandom.RandomRange(-5,5); 
-			direction.y = rand;
+			float randy = TheRandom.RandomRange(-5,5); 
+			direction.y = randy;
 
-			rand = TheRandom.RandomRange(-5,5); 
-			direction.z = rand;
+			float randz = TheRandom.RandomRange(-5,5); 
+			direction.z = randz;
 	
 			TheRigidBody piece_rb = ship_parts[i].GetComponent<TheRigidBody>(); 
 			TheMeshCollider mesh_col = ship_parts[i].GetComponent<TheMeshCollider>(); 
 			
 			//Disable Colliders 
 			ship_parts[i].DestroyComponent(mesh_col); 
-			//mesh_col.SetComponentActive(false); 
 
 			//Modify RigidBody
 			piece_rb.Kinematic = false; 
 			piece_rb.TransformGO = true; 
 			
 			direction = direction.Normalized * explosion_v;
+			
+			//Invert
+			float invert = TheRandom.RandomRange(10,20); 
 
-			TheConsole.Log(direction.x);  
-			TheConsole.Log(direction.y);  
-			TheConsole.Log(direction.z);  
+			if(invert >= 15) 
+				direction *= -1; 
 
-			piece_rb.SetLinearVelocity(direction.x, direction.y, direction.z);		
+			piece_rb.SetLinearVelocity(direction.x, direction.y, direction.z);
+			piece_rb.SetRotation(20,20,20); 		
 		}
 	}
 }
