e55e3d7b01685d222fb467330d6ee8dc4de984be
Roger Tello Araez <rogerteello97@gmail.com>
2018-04-01 14:24:04 +0200

    delete go from shipproperties

--- EngineResources/Project/Assets/Scripts/ShipProperties.cs
+++ EngineResources/Project/Assets/Scripts/ShipProperties.cs
@@ -5,9 +5,6 @@ public class ShipProperties
 {
 
 	private TheScript destruction_scpt;
-	private TheScript game_manager;
-
-	public TheGameObject game_manager_obj; 
 	
 	int hp; 
 	int hp_inc; 
@@ -20,7 +17,6 @@ public class ShipProperties
 		hp_inc = 0; 
 		is_dead = false; 
 		
-		game_manager = game_manager_obj.GetComponent<TheScript>(0);
 		destruction_scpt = TheGameObject.Self.GetComponent<TheScript>(0);   //The number has to be changed by order
 	}
 	
@@ -31,9 +27,9 @@ public class ShipProperties
 
 			destruction_scpt.SetBoolField("need_boom", true);
 
-			game_manager.SetIntField("score_to_inc", 100); 
+			/*game_manager.SetIntField("score_to_inc", 100); 
 			game_manager.CallFunction("AddScore");  
-			game_manager.SetIntField("score_to_inc", 0);
+			game_manager.SetIntField("score_to_inc", 0);*/
 
 			TheConsole.Log("YOU MADE IT"); 
 			is_dead = true; 
