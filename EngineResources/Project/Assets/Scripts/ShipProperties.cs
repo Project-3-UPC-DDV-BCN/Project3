7618e0724d8a42589e8579c68fee3f313b432275
Roger Tello Araez <rogerteello97@gmail.com>
2018-04-04 21:30:11 +0200

    ally kills counting

--- EngineResources/Project/Assets/Scripts/ShipProperties.cs
+++ EngineResources/Project/Assets/Scripts/ShipProperties.cs
@@ -6,6 +6,8 @@ public class ShipProperties
 	private TheScript destruction_scpt;
 	public bool destruction_mesh; 
 	
+	private TheScript game_manager; 
+	
 	int hp; 
 	int hp_inc; 
 
@@ -21,16 +23,31 @@ public class ShipProperties
 
         if(tag != "Alliance" && tag != "Empire")
             destruction_scpt = TheGameObject.Self.GetComponent<TheScript>(1);   
+
+		game_manager = TheGameObject.Find("GameManager").GetComponent<TheScript>(); 
 	}
 	
 	void Update () 
 	{
 		if(hp <= 0 && is_dead == false)
 		{		
+			bool add_ally_kill = false; 
+
 			if(destruction_mesh == true) 
 				destruction_scpt.SetBoolField("need_boom", true);
 
 			is_dead = true; 
+
+			if(TheGameObject.Self.tag == "XWING" || TheGameObject.Self.tag == "YWING")
+				add_ally_kill = true; 
+
+			if(game_manager.GetStringField("team") == "empire")
+				add_ally_kill = !add_ally_kill; 
+
+			if(add_ally_kill)				
+				game_manager.CallFunction("AddAllyKill"); 
+			
+				
 		}
 
 		TheConsole.Log(hp.ToString());
