db35e86724b73c2dd50f8f5c0e44560240fc1660
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-04-04 21:54:12 +0200

    Working with spawners

--- EngineResources/Project/Assets/Scripts/Spawner.cs
+++ EngineResources/Project/Assets/Scripts/Spawner.cs
@@ -1,30 +1,37 @@
 using TheEngine;
 
 public class Spawner 
-{
+{	
+	public string xwing_prefab_name;
+	public string tie_fighter_prefab_name;
+	
 	private TheMeshRenderer ship_mesh; 
-
+	
 	private TheVector3 spawn_position; 
 	private TheGameObject ship_to_spawn;  
 
 	void Start () 
 	{
 			
-		spawn_position = TheGameObject.Self.GetComponent<TheTransform>().GlobalPosition; 
+		spawn_position = TheGameObject.Self.GetComponent<TheTransform>().LocalPosition; 
 		TheGameObject.Self.SetActive(false); 
 
 		if(TheGameObject.Self.tag == "XWING")
 		{
-			ship_to_spawn = TheResources.LoadPrefab("X_Wing(12)");
-			TheGameObject go = TheGameObject.Duplicate(ship_to_spawn); 
-			go.GetComponent<TheTransform>().GlobalPosition = spawn_position; 
+			ship_to_spawn = TheResources.LoadPrefab(xwing_prefab_name);
+			if(ship_to_spawn != null)
+			{
+				ship_to_spawn.GetComponent<TheTransform>().GlobalPosition = spawn_position; 
+			}	
 		} 
 				
 		else if(TheGameObject.Self.tag == "TIEFIGHTER") 
 		{
-			ship_to_spawn = TheResources.LoadPrefab("TieFighterOnePiece");
-            TheGameObject go = TheGameObject.Duplicate(ship_to_spawn);
-            go.GetComponent<TheTransform>().GlobalPosition = spawn_position;
+			ship_to_spawn = TheResources.LoadPrefab(tie_fighter_prefab_name);
+			if(ship_to_spawn != null)
+			{
+            	ship_to_spawn.GetComponent<TheTransform>().LocalPosition = spawn_position;
+			}
         }			
 	}
 	
