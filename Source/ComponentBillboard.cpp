10c75b83a0d31e8c08abaaafc52e550d16b7fed8
Roger Tello Araez <rogerteello97@gmail.com>
2018-04-07 21:50:01 +0200

    Revert "billboard particles"
    This reverts commit 4356a2c57d56d72dd5b74425fd431529c626174e.

--- Source/ComponentBillboard.cpp
+++ Source/ComponentBillboard.cpp
@@ -64,79 +64,75 @@ bool ComponentBillboard::RotateObject()
 	//Get the director vector which the object/particle is currently pointing at (Z axis)
 	ComponentTransform* object_transform;
 
+	float3 reference_axis = { 0,0,-1 };
+
 	if (!attached_to_particle)
 		object_transform = (ComponentTransform*)GetGameObject()->GetComponent(ComponentType::CompTransform);
 	else
 		object_transform = (ComponentTransform*)particle_attached->components.particle_transform;
 
-	float3 object_z = { 0,0,-1 }; 
+	float3 object_z = object_transform->GetMatrix().WorldZ();
+	object_z *= -1;
 
 	//Get the director vector which the object/particle should be pointing at 
 	float3 direction = object_transform->GetGlobalPosition() - reference->camera_frustum.Pos();
 
-	Quat rotation = Quat::RotateFromTo(object_z, direction.Normalized()*-1); 
+	if (billboarding_type == BILLBOARD_Y)
+		direction.y = 0;
 
-	float3 angles_xyz = rotation.ToEulerXYZ() * RADTODEG; 
+	else if (billboarding_type == BILLBOARD_X)
+		direction.x = 0;
 
-	object_transform->SetRotation({ 0,0,0 }); 
-	object_transform->SetRotation(angles_xyz);
+	else if (billboarding_type == BILLBOARD_ALL)
+	{
+		direction = { 0,0,1 };
+		object_transform->SetRotation({ 0, 0, 0 });
+		object_z = {0,0,1};
+	}
+		
 
-	//if (billboarding_type == BILLBOARD_Y)
-	//	direction.y = 0;
+	direction.Normalize();
+	direction *= -1;
 
-	//else if (billboarding_type == BILLBOARD_X)
-	//	direction.x = 0;
+	//Get the angle between where the object is pointing at and where the object should be pointing at in XZ plane
+	float3 desired_pos_projection_xz = { direction.x, 0, direction.z };
+	float3 projection_z_on_xz = { object_z.x, 0, object_z.z };
 
-	//else if (billboarding_type == BILLBOARD_ALL)
-	//{
-	//	direction = { 0,0,1 };
-	//	object_transform->SetRotation({ 0, 0, 0 });
-	//	object_z = {0,0,1};
-	//}
-	//	
+	float angle_xz = desired_pos_projection_xz.AngleBetweenNorm(reference_axis)*RADTODEG;
 
-	//direction.Normalize();
-	//direction *= -1;
+	//Get the angle between where the object is pointing at and where the object should be pointing at in XY plane
+	float3 desired_pos_projection_xy = { 0, direction.y, direction.z };
+	float angle_xy = desired_pos_projection_xy.AngleBetweenNorm(reference_axis)*RADTODEG;
 
-	////Get the angle between where the object is pointing at and where the object should be pointing at in XZ plane
-	//float3 desired_pos_projection_xz = { direction.x, 0, direction.z };
-	//float3 projection_z_on_xz = { object_z.x, 0, object_z.z };
+	if (reference->camera_frustum.Pos().x > 0)
+		angle_xz *= -1;
 
-	//float angle_xz = desired_pos_projection_xz.AngleBetweenNorm(reference_axis)*RADTODEG;
+	if (reference->camera_frustum.Pos().y < 0)
+		angle_xy *= -1;
 
-	////Get the angle between where the object is pointing at and where the object should be pointing at in XY plane
-	//float3 desired_pos_projection_xy = { 0, direction.y, direction.z };
-	//float angle_xy = desired_pos_projection_xy.AngleBetweenNorm(reference_axis)*RADTODEG;
+	switch (billboarding_type)
+	{
+	case BILLBOARD_X:
+		object_transform->SetRotation({ angle_xy, object_transform->GetGlobalRotation().y, object_transform->GetGlobalRotation().z });
+		break;
 
-	//if (reference->camera_frustum.Pos().x > 0)
-	//	angle_xz *= -1;
+	case BILLBOARD_Y:
+		object_transform->SetRotation({ object_transform->GetGlobalRotation().x, angle_xz, object_transform->GetGlobalRotation().z });
+		break;
 
-	//if (reference->camera_frustum.Pos().y < 0)
-	//	angle_xy *= -1;
+	case BILLBOARD_ALL:
+		billboarding_type = BILLBOARD_Y; 
+		RotateObject(); 
 
-	//switch (billboarding_type)
-	//{
-	//case BILLBOARD_X:
-	//	object_transform->SetRotation({ angle_xy, object_transform->GetGlobalRotation().y, object_transform->GetGlobalRotation().z });
-	//	break;
+		billboarding_type = BILLBOARD_X;
+		RotateObject();
 
-	//case BILLBOARD_Y:
-	//	object_transform->SetRotation({ object_transform->GetGlobalRotation().x, angle_xz, object_transform->GetGlobalRotation().z });
-	//	break;
+		billboarding_type = BILLBOARD_ALL; 
 
-	//case BILLBOARD_ALL:
-	//	billboarding_type = BILLBOARD_Y; 
-	//	RotateObject(); 
-
-	//	billboarding_type = BILLBOARD_X;
-	//	RotateObject();
-
-	//	billboarding_type = BILLBOARD_ALL; 
-
-	//	break;
-	//}
-	//	
-	//return ret;
+		break;
+	}
+		
+	return ret;
 }
 
 void ComponentBillboard::Save(Data & data) const
