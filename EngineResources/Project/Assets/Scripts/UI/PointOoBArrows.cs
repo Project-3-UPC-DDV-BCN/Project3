62bccbc943d5fef46ce70cb4e731f61dfee15d66
rcpauor32 <rcolivepau@gmail.com>
2018-04-18 20:27:47 +0200

    OoBArrowPointing implementation

--- EngineResources/Project/Assets/Scripts/UI/PointOoBArrows.cs
+++ EngineResources/Project/Assets/Scripts/UI/PointOoBArrows.cs
@@ -1,11 +1,22 @@
 using TheEngine;
+using TheEngine.TheMath;
 
 public class PointOoBArrows {
 
 	public TheGameObject arrow01;
 	public float radius = 1f;
 
+	public float activeArrowThresholdX = 640f;
+	public float activeArrowThresholdY = 480f;
+
+	public float arrowAngleOffset = 90f;
+
 	TheTransform transform;
+
+	public bool perspective2d = false;
+
+	public float debugX = 0f;
+	public float debugY = 0f;
 
 	void Start () {
 		transform = TheGameObject.Self.GetComponent<TheTransform>();
@@ -15,7 +26,7 @@ public class PointOoBArrows {
 		TheGameObject[] scene_gos = TheGameObject.GetSceneGameObjects();
 		for(int i = 0; i < scene_gos.Length; i++) {
 			if(isShip(scene_gos[i])) {
-				MoveArrowToPosition(scene_gos[i], arrow01);	// Which ships ypu want to point at ?
+				MoveArrowToPosition(scene_gos[i], arrow01);	// Which ships you want to point at ?
 			}
 		}
 	}
@@ -26,14 +37,25 @@ public class PointOoBArrows {
 		// Is Ship in Frustrum ? arrow.active = true : arrow.active = false
 		//	...
 		
-		if(arrow.IsActive() == false || ship.IsActive() == false) return;		
+		
+		//if(arrow.IsActive() == false || ship.IsActive() == false) return;		
 
 		TheRectTransform arrowTrans = arrow.GetComponent<TheRectTransform>();
 		TheTransform ship_transform = ship.GetComponent<TheTransform>();
 		TheVector3 dv2 = ship_transform.GlobalPosition - (transform.GlobalPosition + transform.ForwardDirection.Normalized);
-		dv2 = Coord3DtoCoord2D(dv2, false);
+		dv2 = Coord3DtoCoord2D(dv2, perspective2d);
+		if(TheMath.Abs(dv2.x) > activeArrowThresholdX || TheMath.Abs(dv2.y) > activeArrowThresholdY) {
+			arrow.SetActive(true);
+		}
+		else {
+			arrow.SetActive(false);
+		}
 		dv2.x = -dv2.x;
+		debugX = dv2.x;
+		debugY = dv2.y;
 		arrowTrans.Position = dv2.Normalized * radius;
+		if(dv2.x > 0) arrowTrans.Rotation = new TheVector3(0, 0,-(arrowAngleOffset + TheMath.RadToDeg * TheMath.Atan(dv2.y / -dv2.x)));
+		else arrowTrans.Rotation = new TheVector3(0, 0, arrowAngleOffset + TheMath.RadToDeg * TheMath.Atan(dv2.y / dv2.x));
 	}
 
 	TheVector3 Coord3DtoCoord2D(TheVector3 v3d, bool w_perspective = false) {
@@ -47,4 +69,5 @@ public class PointOoBArrows {
 			return true;
 		return false;
 	}
+
 }
