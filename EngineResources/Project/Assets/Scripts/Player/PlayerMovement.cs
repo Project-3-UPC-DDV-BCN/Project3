34c2bdb7e88f6a247ad3b609ca24cbd0ef4a3b72
Trodek <ibanmas@gmail.com>
2018-04-19 23:05:48 +0200

    player movement wip

--- EngineResources/Project/Assets/Scripts/Player/PlayerMovement.cs
+++ EngineResources/Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -44,6 +44,19 @@ public class PlayerMovement {
 	private float curr_accel;
 	///slow
 	private bool slowing = false;
+	/// camera movement
+	public float roll_camera_rot = 10.0f;
+	public float pitch_camera_rot = 10.0f;
+	public float yaw_camera_rot = 10.0f;
+	public float max_camera_displacement = 0.1f;
+    public float camera_rot_step = 1.0f;
+	public float camera_return_step = 2.0f;
+    public TheGameObject camera_go;
+    private TheVector3 original_cam_rot;
+    private TheVector3 original_cam_pos;
+    private TheVector3 cam_rot;
+    private TheVector3 cam_pos;
+	
 	
 	// Ship Information Timers
 	///boost
@@ -107,14 +120,225 @@ public class PlayerMovement {
 	
 	//Audio
 	public TheGameObject audio_emiter;
-	TheAudioSource audio_source;
+	private TheAudioSource audio_source;
 	
+	//Particle Emitter for speed fx
+	public TheGameObject particle_emitter_go;
+	private TheParticleEmmiter particle_emitter = null;
+	public float particle_base_speed = 1.0f;
+	
+	//Current frame delta time
+	private float delta_time = 0.0f;
 
-	void Start () {
+	void Start () 
+	{
+		//Get the transform
+		trans = TheGameObject.Self.GetComponent<TheTransform>();
+		
+		//Get all the Components needed for the UI elements
+		/// Bars
+		if(weapons != null)
+			weapons_bar = weapons.GetComponent<TheProgressBar>();
+	
+		if(shields != null)
+			shields_bar = shields.GetComponent<TheProgressBar>();
+
+		if(energy != null)
+			energy_bar = energy.GetComponent<TheProgressBar>();
+
+		if(speed != null)
+			speed_bar = speed.GetComponent<TheProgressBar>();
+
+		if(shield_hp_go != null)
+        	shield_hp_bar = shield_hp_go.GetComponent<TheProgressBar>();
+
+		if(hp != null)
+       		hp_bar = hp.GetComponent<TheProgressBar>();
+
+		if(weapons_bar != null)
+       		weapons_bar.PercentageProgress = 100;
+
+		if(shields_bar != null)
+			shields_bar.PercentageProgress = 100;
+
+		if(energy_bar != null)
+			energy_bar.PercentageProgress = 100;
+		/// Images
+		if(inner_ring != null)
+		{
+       		inner_ring_trans = inner_ring.GetComponent<TheRectTransform>();
+			inner_ring.SetActive(false);
+		}
+		
+		if(center_ring != null)
+		{
+       		center_ring_trans = center_ring.GetComponent<TheRectTransform>();
+       		center_ring.SetActive(false);
+		}
+
+		if(exterior_ring != null)
+		{
+       		exterior_ring_trans = exterior_ring.GetComponent<TheRectTransform>();
+       	 	exterior_ring.SetActive(false);
+		}
+		
+		if(selected_inner_ring != null)
+		{
+			selected_inner_ring_trans = selected_inner_ring.GetComponent<TheRectTransform>();
+			selected_inner_ring.SetActive(false);
+		}
+
+		if(selected_center_ring != null)
+		{
+       		selected_center_ring_trans = selected_center_ring.GetComponent<TheRectTransform>();
+			selected_center_ring.SetActive(false);
+		}
+
+		if(selected_exterior_ring != null)
+		{
+       		selected_exterior_ring_trans = selected_exterior_ring.GetComponent<TheRectTransform>();
+			selected_exterior_ring.SetActive(false);
+		}
+		
+		//Energy System Start
+		max_energy *= 2;
+		shield_energy *= 2;
+		weapon_energy *= 2;
+		engine_energy *= 2;
+		max_energy_on_system *= 2;
+		
+		//Set initial values for velocity and acceleration
+		curr_accel = acceleration;
+		curr_max_vel = max_vel;
+		
+		//HP and shield setup
+		body_hp = wings_hp = engine_hp = total_hp/3.0f;
+		curr_total_hp = total_hp;
+		curr_shield_hp = shield_hp;
+        shield_regen_energy = shield_regen;
+		
+		//Camera information
+		if(camera_go != null)
+		{
+        	original_cam_pos = camera_go.GetComponent<TheTransform>().LocalPosition;
+        	original_cam_rot = camera_go.GetComponent<TheTransform>().LocalRotation;
+		}
+        cam_pos = new TheVector3(0, 0, 0);
+        cam_rot = new TheVector3(0, 0, 0);
+		
+		//Audio 
+		if(audio_emiter != null)
+		{
+       		audio_source = audio_emiter.GetComponent<TheAudioSource>();
+
+			if(audio_source != null)
+			{
+				audio_source.Play("Play_Engine");
+				audio_source.SetMyRTPCvalue("Speed",vel_percent);
+			}
+		}
+		
+		//Particle Emitter
+		if(particle_emitter_go != null)
+		{
+			particle_emitter = particle_emitter_go.GetComponent<TheParticleEmmiter>();
+		}
+	}
+	
+	void Update () 
+	{
+		delta_time = TheTime.DeltaTime;
 		
 	}
 	
-	void Update () {
+	void Movement()
+	{
 		
 	}
+	
+	void SetValuesFromEnergy()
+	{
+		// Calculate current max velocity form energy_bar
+		curr_max_vel = 0.5f * max_vel + ((12.5f*(float)engine_energy)/100) * max_vel;
+		// Calculate current shield regen from energy_bar
+        shield_regen_energy = 0.5f * shield_regen + ((12.5f * (float)shield_energy) / 100) * shield_regen;
+    }
+	
+	void EnergyManagement()
+	{
+		if(TheInput.IsKeyDown("UP_ARROW"))
+		{
+			audio_source.Stop("Play_droid_speed_up");
+			audio_source.Play("Play_droid_speed_up");
+			if(shield_energy < max_energy_on_system-1)
+			{
+				shield_energy += 2;
+				if(weapon_energy > 0 && engine_energy > 0)
+				{
+					weapon_energy -= 1;
+					engine_energy -= 1;
+				}
+				else if(weapon_energy > 2)
+					weapon_energy -= 2;
+				
+				else 
+					engine_energy -=2;
+			}
+		}
+		
+		if(TheInput.IsKeyDown("LEFT_ARROW"))
+		{
+			audio_source.Stop("Play_Shield_up");
+			audio_source.Play("Play_Shield_up");
+			if(weapon_energy < max_energy_on_system-1)
+			{
+				weapon_energy += 2;
+				if(shield_energy > 0 && engine_energy > 0)
+				{
+					shield_energy -= 1;
+					engine_energy -= 1;
+				}
+				else if(shield_energy > 2)
+					shield_energy -= 2;
+
+				else 
+					engine_energy -=2;
+			}
+		}
+
+		if(TheInput.IsKeyDown("RIGHT_ARROW"))
+		{
+			audio_source.Stop("Play_Potency_up");
+			audio_source.Play("Play_Potency_up");
+			if(engine_energy < max_energy_on_system-1)
+			{
+				engine_energy += 2;
+				if(shield_energy > 0 && weapon_energy > 0)
+				{
+					shield_energy -= 1;
+					weapon_energy -= 1;
+				}
+				else if(shield_energy > 2)
+					shield_energy -= 2;
+				else 
+					weapon_energy -=2;
+			}
+		}
+
+		if(TheInput.IsKeyDown("DOWN_ARROW"))
+		{
+			audio_source.Stop("Play_droid_speed_down");
+			audio_source.Play("Play_droid_speed_down");
+			shield_energy = 4;
+			weapon_energy = 4;
+			engine_energy = 4;
+		}
+
+		if(TheInput.IsKeyDown("F1"))
+		{
+			DamageSlaveOne(10.0f);
+		}
+	}
+	
+	
 }
