62c79a32b22afa4b6028a7d03fddaf5d515c725a
Trodek <ibanmas@gmail.com>
2018-04-22 12:39:52 +0200

    updated on hit audio for player

--- EngineResources/Project/Assets/Scripts/Player/PlayerMovement.cs
+++ EngineResources/Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -721,7 +721,7 @@ public class PlayerMovement {
 	public void DamageSlaveOne(float dmg)
     {
 		if(audio_source != null)
-			audio_source.Play("Play_Player_hit");
+			audio_source.Play("Play_Ship_hit)";
 
         switch(TheRandom.RandomInt() % ship_parts)
         {
