640d3dcfadae7148312ef9a1820fc64418e7f590
SimonStoyanov <simon.stoyanov@hotmail.com>
2018-04-22 20:58:49 +0200

    Optimized code for AIMovement and StarshipShooting and changed the AI prefab and scene

--- EngineResources/Project/Assets/Scripts/Player/Weapon1.cs
+++ EngineResources/Project/Assets/Scripts/Player/Weapon1.cs
@@ -18,11 +18,12 @@ public class Weapon1
 	TheTimer heat_timer = new TheTimer();
 	public float heat_time = 2.0f;
 	
+	public TheGameObject game_manager = null;
+	
 	void Start()
 	{
 		starship_shooting = TheGameObject.Self.GetScript("VS4StarShipShooting");
 
-		TheGameObject game_manager = TheGameObject.Find("GameManager");
 		if(game_manager != null)
 		{
 			TheScript game_manager_script = game_manager.GetScript("GameManager");
