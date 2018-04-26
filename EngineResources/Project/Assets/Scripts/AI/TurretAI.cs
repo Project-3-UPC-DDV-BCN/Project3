405ef6d406d57d75508755e33c185c9e8c6bfe03
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2018-04-26 12:30:47 +0200

    Added base code to start with the AITurret in the future

--- EngineResources/Project/Assets/Scripts/AI/TurretAI.cs
+++ EngineResources/Project/Assets/Scripts/AI/TurretAI.cs
@@ -1,9 +1,16 @@
 using TheEngine;
 
 public class TurretAI {
+	
+	TheGameObject TurretBase = null;	
+	public TheGameObject TurretHead	= null;
+	public TheGameObject BlasterPivot = null;
+	public TheGameObject BlasterCannon = null;
+	TheFactory blaster_factory = null;	
 
 	void Start () {
-		
+		TurretBase = TheGameObject.Self;
+		blaster_factory	= BlasterCannon.GetComponent<TheFactory>();
 	}
 	
 	void Update () {
