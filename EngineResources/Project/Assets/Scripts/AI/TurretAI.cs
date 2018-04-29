c3c8769d5935bf224af1f2404bc7e5a8aecd1d98
SimonStoyanov <simon.stoyanov@hotmail.com>
2018-04-29 11:50:20 +0200

    Base code por the AI Turret

--- EngineResources/Project/Assets/Scripts/AI/TurretAI.cs
+++ EngineResources/Project/Assets/Scripts/AI/TurretAI.cs
@@ -4,16 +4,58 @@ public class TurretAI {
 	
 	TheGameObject TurretBase = null;	
 	public TheGameObject TurretHead	= null;
+	TheTransform SelfTransform = null;
+	TheVector3 SelfPosition;
 	public TheGameObject BlasterPivot = null;
+	TheTransform BlasterTransform = null;
 	public TheGameObject BlasterCannon = null;
-	TheFactory blaster_factory = null;	
+	TheFactory blaster_factory = null;
+
+	public TheGameObject TargetPlayer = null;
+	TheTransform PlayerTransform = null;
+	TheVector3 PlayerPosition;
+	
+	float DeltaTime = 0.0f;
+	public float MinDistance = 0.0f;
+	public float MinAngleBlasters = 0.0f;
+	public float MaxAngleBlasters = 60.0f;
 
 	void Start () {
 		TurretBase = TheGameObject.Self;
+		BlasterTransform = BlasterPivot.GetComponent<TheTransform>();
+		PlayerTransform = TargetPlayer.GetComponent<TheTransform>();
+		SelfTransform = TurretHead.GetComponent<TheTransform>();
+		
 		blaster_factory	= BlasterCannon.GetComponent<TheFactory>();
 	}
 	
 	void Update () {
+		PlayerPosition = PlayerTransform.GlobalPosition;
+		SelfPosition = SelfTransform.GlobalPosition;
+		DeltaTime = TheTime.DeltaTime;
+		
+		if (TheVector3.Distance(PlayerPosition, SelfPosition) < MinDistance)
+		{
+			RotateHeadTowardsPlayer();
+			RotateBlasterTowardsPlayer();
+			Shoot();
+		}	
+	}
+	
+	void RotateHeadTowardsPlayer()
+	{
+		// Face Forward Dir to PlayerPosition
+		
+	}
+	
+	void RotateBlasterTowardsPlayer()
+	{
+		// Face Blasters to the PlayerPosition
+		
+	}
+	
+	void Shoot()
+	{
 		
 	}
 }
