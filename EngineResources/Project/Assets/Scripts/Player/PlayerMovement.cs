2024ac04922205d72a0d43e9bf7d275ec03f9cad
Sergi Pérez <sergiperezandreu@gmail.com>
2018-05-09 09:28:33 +0200

    Repair puzzle now shows the percentage of each part

--- EngineResources/Project/Assets/Scripts/Player/PlayerMovement.cs
+++ EngineResources/Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -867,13 +867,13 @@ public class PlayerMovement
 		
 		if(engine_part_hp_text != null && wing_part_hp_text != null && body_part_hp_text != null)
 		{
-			int tmp_body_int = (int)(body_hp * 3);
-			int tmp_wing_int = (int)(wings_hp * 3);
-			int tmp_engine_int = (int)(engine_hp * 3);
+			int tmp_body_int = (int)(body_hp * 2 - 3);
+			int tmp_wing_int = (int)(wings_hp * 2 - 3);
+			int tmp_engine_int = (int)(engine_hp * 2 - 3);
 			
-			body_part_hp_text.Text = tmp_body_int.ToString();
-			wing_part_hp_text.Text = tmp_wing_int.ToString();
-			engine_part_hp_text.Text = tmp_engine_int.ToString();
+			body_part_hp_text.Text = tmp_body_int.ToString() + "%";
+			wing_part_hp_text.Text = tmp_wing_int.ToString() + "%";
+			engine_part_hp_text.Text = tmp_engine_int.ToString() + "%";
 		}
 	}
 	
