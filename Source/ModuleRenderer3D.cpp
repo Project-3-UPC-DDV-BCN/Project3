814969065cd0e863603621097431cf6115a38e91
JosepUPC <rchuguetgironjosep@gmail.com>
2018-06-02 13:00:06 +0200

    Fixed blending for lasers

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -776,11 +776,12 @@ void ModuleRenderer3D::DrawSceneGameObjects(ComponentCamera* active_camera, bool
 
 	current_material = nullptr;
 
+	glDepthMask(GL_FALSE);
 	for (std::map<float, ComponentMeshRenderer*>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
 	{
 		DrawMeshWithBlending(it->second, active_camera);
 	}
-
+	glDepthMask(GL_TRUE);
 
 	for (uint i = 0; i < cubes_to_draw.size(); ++i)
 	{
